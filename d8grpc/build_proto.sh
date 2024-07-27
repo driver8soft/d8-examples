e37c9b268d7795e61b9676a8d901da97b09e5795
josemr3101 <jmr@driver8soft.com>
2024-07-27 16:42:15 +0200

    Removed git ignored files

--- d8grpc/build_proto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export GOPATH=$(go env GOPATH)
-
-export PATH=$PATH:$GOPATH/bin
-
-protoc --go_out=. --go_opt=paths=source_relative \
-    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    hello/hello.proto
--- d8grpc/hello_server/cobol_path.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export COB_LIBRARY_PATH=/Users/jmruesta/go/src/examples/d8grpc/hello_server/libs
-
-
