903767eb29245b50ee22b2fbc3344db4be203682
josemr3101 <jmr@driver8soft.com>
2024-08-08 21:22:39 +0200

    python example

--- hello-py/hello_pb2.py
+++ hello-py/hello_pb2.py
@@ -14,14 +14,14 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bhello.proto\x12\x05hello\"\x1c\n\x06MsgReq\x12\x12\n\nhello_name\x18\x01 \x01(\t\"\x1a\n\x06MsgRes\x12\x10\n\x08response\x18\x01 \x01(\t21\n\x06\x44\x38grpc\x12\'\n\x05Hello\x12\r.hello.MsgReq\x1a\r.hello.MsgRes\"\x00\x42\x16Z\x14\x65xamples/hello/hellob\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0bhello.proto\x12\x05hello\"\x1c\n\x06MsgReq\x12\x12\n\nhello_name\x18\x01 \x01(\t\"\x1a\n\x06MsgRes\x12\x10\n\x08response\x18\x01 \x01(\t21\n\x06\x44\x38grpc\x12\'\n\x05Hello\x12\r.hello.MsgReq\x1a\r.hello.MsgRes\"\x00\x42.Z,github.com/driver8soft/examples/d8grpc/hellob\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'hello_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
-  _globals['DESCRIPTOR']._serialized_options = b'Z\024examples/hello/hello'
+  _globals['DESCRIPTOR']._serialized_options = b'Z,github.com/driver8soft/examples/d8grpc/hello'
   _globals['_MSGREQ']._serialized_start=22
   _globals['_MSGREQ']._serialized_end=50
   _globals['_MSGRES']._serialized_start=52
